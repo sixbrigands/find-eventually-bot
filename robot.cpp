44d60a140e4fe90f4e0ad49395764c52de7dc80b
Dylan Callahan <45744428+sixbrigands@users.noreply.github.com>
2019-12-11 01:21:26 -0500

    Total change of direction(ha)
    I finally gave up on the maze idea.  The dc motors with no guidance are simply too unreliable to accurately retrace steps.  It was a good concept, but needs better equipment (steppers, perhaps more USDS)
    
    The new function is to locate the IR light in a walled space and stop when it reaches it.  It does this pretty slowly, but pretty accurately.

--- robot.cpp
+++ robot.cpp
@@ -5,8 +5,10 @@
 #define CM 1 //for instantiaion of USsensor
 #define echo p27
 #define trig p28
-#define lightThreshold 10.0 //the light level to indicate goal is reached
+#define lightThreshold 15.0 //the light level to indicate goal is reached
 #define cycle 0.1 // time increment to for driving
+
+
  
 DigitalOut Bin1(p5);
 DigitalOut Bin2(p6);
@@ -22,52 +24,60 @@ DigitalOut led3(LED3);
 DigitalOut led4(LED4);
 AnalogIn IRsensor(p15);
 
-int turnedRight = 0; //two counters to keep track of turning
-int turnedLeft = 0;
-int startedDriving = 0; // turn this state to one when we start driving
-vector<int> travelLog; //saves travel commands in reverse for backtrack
 
 
 
 void drive(float speed) {        //go forward with a speed from 0 to 1
-    speedA = speed; 
+    speedA = speed + 0.01; 
     speedB = speed;
     Bin1 = 0; 
     Bin2 = 1;
     Ain1 = 0;
     Ain2 = 1;
     standby = 1; //turn motors on
-  }
+}
 
 void reverse(float speed) {        //go backwards with a speed from 0 to 1
-    speedA = speed; 
+    speedA = speed + 0.01; 
     speedB = speed;
     Bin1 = 1; 
     Bin2 = 0;
     Ain1 = 1;
     Ain2 = 0;
     standby = 1; //turn motors on
-  }
+}
 
 void stop() {        //stop
     standby = 0; //turn motors off
-  }
+}
 
 void turnRight() { //turns 90 degress 
-    speedA = 0.5f; //half speed for turns
-    speedB = 0.5f;
+    speedA = 0.5;
+    speedB = 0.5;
     Bin1 = 0;
     Bin2 = 1;
     Ain1 = 1;
     Ain2 = 0;
     standby = 1; //turn motors on
-    wait(.515); //give it time to turn gtg
+    wait(.525); //give it time to turn gtg
+    standby = 0; //turn motors off
+}
+
+void halfRight() { //turns 90 degress 
+    speedA = 0.5;
+    speedB = 0.5;
+    Bin1 = 0;
+    Bin2 = 1;
+    Ain1 = 1;
+    Ain2 = 0;
+    standby = 1; //turn motors on
+    wait(.525); //give it time to turn gtg
     standby = 0; //turn motors off
 }
 
 void turnLeft() {  //turns 90 degress
-    speedA = 0.5f; //half speed for turns
-    speedB = 0.5f;
+    speedA = 0.5; 
+    speedB = 0.5;
     Bin1 = 1;
     Bin2 = 0;
     Ain1 = 0;
@@ -77,86 +87,124 @@ void turnLeft() {  //turns 90 degress
     standby = 0; //turn motors off
 }
 
-float getAvg(float buf[]){ //used for getting the average of lightBuffer[]
-    float sum = 0;
-    for(int i = 0; i < 5; i++){
-        sum = sum + buf[i];
-    }
-    return sum/5;
+void halfLeft() {  //turns 90 degress
+    speedA = 0.5; 
+    speedB = 0.5;
+    Bin1 = 1;
+    Bin2 = 0;
+    Ain1 = 0;
+    Ain2 = 1;
+    standby = 1; //turn motors on
+    wait(.569); //give it time to turn
+    standby = 0; //turn motors off
 }
 
-/*the for backtrack are:
-1 = drive (next element tells you how many cycles to do so)
-2 = turn right
-3 = turn left */
+float getAvgLight(){ //used for getting the average of lightBuffer[]
+    float sum = 0;
+    int readings = 100;
+    for(int i = 0; i < readings; i++){
+        sum = sum + (IRsensor * 10000);
+    }
+    return sum/readings;
+}
 
-void backtrack(){
-    turnRight();
-    turnRight(); //first turn 180
 
-    for (int i = travelLog.size() - 1; i >= 0; i--){ //iterate backwards
-        switch (travelLog.at(i)){
-        case 1: 
-        drive(0.5);
-        wait(travelLog.at(i - 1) * cycle); //how far to go
-        i--; 
+void look360(){  //looks around and turns robot in direction of highest light
+    float minReading = 1000;
+    int direction;
+
+    for(int i = 0; i < 4; i++){ //get light readings for cardinal directions
+        if(getAvgLight() < minReading) {
+            printf("360 Direction %d reads %f\n\r", i, getAvgLight());
+            minReading = getAvgLight();
+            direction = i;
+        }
+        turnRight();
+    }
+    switch (direction){
+        case 1: turnRight();
             break;
         case 2: turnRight();
+                turnRight();
             break;
         case 3: turnLeft();
-        default: stop();
+        default:;
         }
+}
+
+void look90(){ // get light readings for current direction and 45degres left and right, face robot that directio
+    float minReading = getAvgLight();
+    int direction = 0;
+    halfLeft();
+    if(getAvgLight() < minReading) {
+            minReading = getAvgLight();
+            direction = 1;
     }
-    stop();
+    turnRight();
+    if(getAvgLight() < minReading) {
+            minReading = getAvgLight();
+            direction = 2;
+    }
+    switch (direction){
+        case 0: halfLeft();
+            break;
+        case 1: turnLeft();
+            break;
+        default:;
+    }                
 }
 
 
 int main() {
-    powerForUSDC = 1;
     HCSR04 USsensor(trig, echo); //USDS setup
-    float lightBuffer[5] = {100, 100, 100, 100, 100}; //buffer for light readings
-    int i = 0; // and index for the buffer 
-    
+    powerForUSDC = 1;
+    look360(); //do an intial lookaound
+    int i = 0;
 
-    while (getAvg(lightBuffer) > lightThreshold){ //continue this loop as long as not near IR light
+    while (1){ //continue this loop as long as not near IR light
+        i++;
+        i = i % 10;
+
         long distance = USsensor.distance(CM); //get distance from obstacle in cm, reports - 1 if it fails to get distance
         if( distance < 0) { distance = 100;}  //just set readings of -1 to 100
-        float light = IRsensor * 10000; //IR light reading, in normal light produces value around 60-70
-        lightBuffer[i] = light;
-        i = i % 5; // make i wrap around to 0 when it hits he 5th element (ha)
-        printf("Distance %d\n\r", distance); //distance returns -1 if it can't get it
-        //printf("Light %f\n\r", light); 
 
+        if ((getAvgLight() < lightThreshold) && (distance <= 8)) { //the goal, close to the light
+            stop();
+            printf("Done!!!! %f\n\r", getAvgLight());
+            break;
+        } 
 
-        //navigation
-        if (distance < 4) {
-            startedDriving = 0; //you've turned, reset the startedDriving state
-            turnRight();
-            travelLog.push_back(3);
+        if (getAvgLight() < lightThreshold) { //approach light slowly
+            if (distance > 9) {
+                drive(.3);
+                printf("Approching light %f\n\r", getAvgLight());
+                wait(.3);
+            }
+            else{
+                printf("Done!!!! %f\n\r", getAvgLight());
+                break;
+            }
+        
         }
 
-        if (turnedRight > 1) { //if you've turned right twice you are backwards, do one more turn.
-            turnRight();
-            travelLog.push_back(3); 
-            turnedRight = 0; //reset the state
+
+        if(distance < 11) {
+            printf("I hit a wall %f\n\r", getAvgLight());
+            reverse(.5);
+            wait(.9);
+            stop();
+            look360();
         }
 
+        if((i == 0)  && (getAvgLight() > lightThreshold)) { // every 20th cycle take a quick look;
+        printf("Look90 %f\n\r", getAvgLight());
+        look90();
+        } 
 
-        if (startedDriving){   //if we've already been driving, increment the number of cycles we've been doing so 
-            travelLog.at(1)++; //the number of cycles is second from the front
-            drive(0.5);
-        }
-
-        if (!startedDriving){
-            travelLog.push_back(1); //this logs how many cycles we've been driving
-            travelLog.push_back(1); //this logs the commmand for driving
-            drive(0.5);
-            //now the vector should be [...#ofCycles, 1]
-        }
-        wait(0.1);  
+        drive(0.5);
+        wait(cycle);  
     }
 
-    backtrack(); //start the backtrack!
 
     
 }
